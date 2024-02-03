25a850e35045160d66e97255f131132bdc73c501
Narendra <todatnarendra@gmail.com>
2024-02-02 22:01:13 -0800

    todo list with component design

---
+++ src/component/Todolist.jsx
@@ -0,0 +1,22 @@
+const Todolist = () => {
+  return (
+    <>
+      <div className="row Nk-list">
+        <div className="col-4">Narendra Kumar</div>
+        <div className="col-4 ">20/11/1989</div>
+        <div className="col-4 ">
+          <button className="btn btn-danger nk-button">Delete </button>
+        </div>
+      </div>
+      <div className="row Nk-list">
+        <div className="col-4">Narendra Kumar</div>
+        <div className="col-4 ">20/11/1989</div>
+        <div className="col-4 ">
+          <button className="btn btn-danger nk-button">Delete </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Todolist;
---
+++ src/component/Todotitle.jsx
@@ -0,0 +1,11 @@
+import titlecss from './Todotitle.module.css'
+const Todotitle = () => {
+
+   return (
+    <>
+    <h1 className={titlecss['Nk-title']}>ToDo List</h1>
+    </>
+   )
+
+}
+export default Todotitle;
