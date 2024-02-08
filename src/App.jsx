e8f59a78fbdabfb0c168b36fbe172597c103a2a1
Narendra <todatnarendra@gmail.com>
2024-02-08 01:38:07 -0800

    todo list

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
+import React from 'react';
 import Todotitle from "./component/Todotitle";
 import Addtodo from "./component/Addtodo";
 import Todolist from "./component/Todolist";
-import '../src/App.css';
+import "../src/App.css";
 
-function App (){
+function App() {
+//  let todos= [];
+  let todos = [
+    { name: "Narendra Kumar", dob: "20/10/1990" },
+    { name: "Amit Kumar", dob: "24/5/1993" },
+    { name: "Sumit Kumar", dob: "20/10/1996" },
+    { name: "Priyanka Kumar", dob: "20/11/1998" },
+    { name: "Lalit Kumar", dob: "20/2/1999" },
+  ];
   return (
     <div className="container center">
-    <Todotitle></Todotitle>
-    <Addtodo></Addtodo>
-    <Todolist></Todolist>
+      <Todotitle></Todotitle>
+      <Addtodo></Addtodo>
+      <Todolist todoData={todos}></Todolist>
     </div>
-
-  )
-
+  );
 }
 export default App;
