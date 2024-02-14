aed8d342c8076ec3bc6570a0972fd2452fc2a755
Narendra <todatnarendra@gmail.com>
2024-02-14 03:36:43 -0800

    module.css

--- src/component/Addtodo.jsx
+++ src/component/Addtodo.jsx
@@ -1,10 +1,13 @@
+import addcss from './addtodo.module.css'
 const Addtodo = () => {
+
+  console.log();
   return (
     <>
       <div class="row">
         <div class="col-4"><input type="text" placeholder="input todo" class="form-control" / ></div>
         <div class="col-4 "><input type="date"  class="form-control" / ></div>
-        <div class="col-4 "><button class="btn btn-primary nk-button">Add </button></div>
+        <div class="col-4 "><button class={`${addcss['nk-button']} btn btn-primary`}>Add </button></div>
       </div>
     </>
   );
