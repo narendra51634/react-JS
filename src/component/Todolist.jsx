87e3991ddaeccd0fd9e0d363b0aef9fbd6398a5a
Narendra <todatnarendra@gmail.com>
2024-02-23 03:49:43 -0800

    New file for store addded

--- src/component/Todolist.jsx
+++ src/component/Todolist.jsx
@@ -1,25 +1,24 @@
-import React from 'react';
+import React from "react";
 const Todolist = (props) => {
-  
   let tododata = props.todoData;
   return (
     <>
-    {  tododata.length === 0 ? 'Data not Exist':null   }
+      {tododata.length === 0 ? "Data not Exist" : null}
 
-    {tododata.map((item) =>
-    
-        <div key={{item}} className="row Nk-list">
-        <div className="col-4">{item.name}</div>
-        <div className="col-4 ">{item.dob}</div>
-        <div className="col-4 ">
-          <button onClick={()=> props.deletetodo(item.name)} className="btn btn-danger nk-button">Delete </button>
+      {tododata.map((item) => (
+        <div key={{ item }} className="row Nk-list">
+          <div className="col-4">{item.name}</div>
+          <div className="col-4 ">{item.dob}</div>
+          <div className="col-4 ">
+            <button
+              onClick={() => props.deletetodo(item.name)}
+              className="btn btn-danger nk-button"
+            >
+              Delete{" "}
+            </button>
+          </div>
         </div>
-      </div>
-      )}
-    
-      
-      
-     
+      ))}
     </>
   );
 };
