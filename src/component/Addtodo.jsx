5611bffe10874c483edff848624e93dcf47a36a9
Narendra <todatnarendra@gmail.com>
2024-02-23 03:05:47 -0800

    todo doe

--- src/component/Addtodo.jsx
+++ src/component/Addtodo.jsx
@@ -1,13 +1,53 @@
-import addcss from './addtodo.module.css'
-const Addtodo = () => {
+import { useState } from "react";
+import addcss from "./addtodo.module.css";
+const Addtodo = ({ hendelAddTodo }) => {
+  const [inputVal, setInputVal] = useState("");
 
-  console.log();
+  const [inputDate, setInputDate] = useState("");
+
+  const hendelInputValue = (event) => {
+    setInputVal(event.target.value);
+  };
+
+  const hendelDateValue = (event) => {
+    setInputDate(event.target.value);
+  };
+
+  const hendelAddTododata = () => {
+    let tododata = { name: inputVal, dob: inputDate };
+    hendelAddTodo(tododata);
+    setInputVal("");
+    setInputDate("");
+  };
+
   return (
     <>
-      <div class="row">
-        <div class="col-4"><input type="text" placeholder="input todo" class="form-control" / ></div>
-        <div class="col-4 "><input type="date"  class="form-control" / ></div>
-        <div class="col-4 "><button class={`${addcss['nk-button']} btn btn-primary`}>Add </button></div>
+      <div className="row">
+        <div className="col-4">
+          <input
+            type="text"
+            value={inputVal}
+            placeholder="input todo"
+            className="form-control"
+            onChange={hendelInputValue}
+          />
+        </div>
+        <div className="col-4 ">
+          <input
+            type="date"
+            value={inputDate}
+            onChange={hendelDateValue}
+            className="form-control"
+          />
+        </div>
+        <div className="col-4 ">
+          <button
+            onClick={hendelAddTododata}
+            className={`${addcss["nk-button"]} btn btn-primary`}
+          >
+            Add
+          </button>
+        </div>
       </div>
     </>
   );
